589370163f54f0c9c1a07515e0bacc9d477145f8
tarikm2 <tarikm2@uw.edu>
2015-11-21 11:39:51 +0000

    facebook api test

---
+++ esteam/app/assets/javascripts/facebookSDK.js
@@ -0,0 +1,16 @@
+  //this is the javascript for linking to the facebook api
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '908111849271091',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
